9cac0a8cce95cad6850837af1ec83e266fca2cd2
hbJeSeong <hbjs9443@naver.com>
2023-10-22 19:18:25 +0900

    Init Project

---
+++ src/main/java/com/jshwangbo/INO_SERVER/InoServerApplication.java
@@ -0,0 +1,13 @@
+package com.jshwangbo.INO_SERVER;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InoServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InoServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jshwangbo/INO_SERVER/InoServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jshwangbo.INO_SERVER;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InoServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
